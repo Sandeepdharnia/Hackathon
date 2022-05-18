3b137d1a7620039a959da124c8b97619b94c7c57
DanK1368 <danielkoett@gmail.com>
2022-05-18 15:33:14 +0200

    added solution 09 merge

--- 09_merge.js
+++ 09_merge.js
@@ -1,7 +1,13 @@
-const merge = object => {
-  let newObject = {};
-  for(let i=0; i < values.length)
+const merge = (...objects) => {
+  const newObject = {};
+  for (let i = 0; i < objects.length; i++) {
+    Object.keys(objects[i]).forEach(key => {
+      if (!newObject[key] || newObject[key] === 0)
+        newObject[key] = objects[i][key];
+    });
+  }
+  return newObject;
 };
 
-// merge({ a: 3, b: 2 }, { a: 2, c: 4 }); // { a: 3, b: 2, c: 4 }
-merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5 }); // { a: 3, b: 2, c: 4, e: 8 }
+console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 })); // { a: 3, b: 2, c: 4 }
+console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5 })); // { a: 3, b: 2, c: 4, e: 8 }
